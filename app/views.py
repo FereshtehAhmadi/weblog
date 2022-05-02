e581c48943841ab1a4706d163fb6a9ae29eac36b
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-05-02 21:54:53 +0430

    login user

--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Person
 
-# Create your views here.
+def home(request):
+    return render(request, 'app/home.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        userName = request.POST['username']
+        password = request.POST['password']
+        if Person.objects.filter(userName=userName).exists() == True:
+            obj = Person.objects.get(userName=userName)
+            if obj.password == password:
+                return redirect('home')
+    return render(request, 'app/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        userName = request.POST['username']
+        firstName = request.POST['fname']
+        lastName = request.POST['lname']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        p = Person(userName=userName, firstName=firstName, lastName=lastName, password=password, email=email)
+        p.save()
+        return redirect('login')
+    
+    return render(request, 'app/register.html')
